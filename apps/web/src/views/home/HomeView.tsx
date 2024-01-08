7e10727b421351e2d69d50fe2f7f7a995749eca0
JUtupe <jutuper@gmail.com>
2024-01-08 23:06:13 +0100

    fix: home view gap

--- apps/web/src/views/home/HomeView.tsx
+++ apps/web/src/views/home/HomeView.tsx
@@ -1,48 +1,61 @@
 import React from "react";
-import {useUser} from "../../context/user/UserContext.hook";
-import Navbar from "../../component/organisms/navbar/Navbar";
-import NotFoundSvg from "../../assets/images/not-found.svg"
-import styles from "./HomeView.module.scss"
-import {Button} from "../../component/atoms/button/Button";
-import CreateTeamSvg from "../../assets/icons/create-team.svg";
 import {useNavigate} from "react-router";
+import CreateTeamSvg from "../../assets/icons/create-team.svg";
+import NotFoundSvg from "../../assets/images/not-found.svg";
+import {Button} from "../../component/atoms/button/Button";
+import {Card} from "../../component/molecules/card/Card";
 import {TeamRetroList} from "../../component/molecules/team_retro_list/TeamRetroList";
+import Navbar from "../../component/organisms/navbar/Navbar";
+import {useUser} from "../../context/user/UserContext.hook";
+import styles from "./HomeView.module.scss";
 
 const HomeView: React.FC = () => {
-    const { user } = useUser();
-    const navigate = useNavigate()
+	const { user } = useUser();
+	const navigate = useNavigate();
 
-    return (
-        <>
-            <Navbar/>
-            <div className={styles.container}>
-                {user?.teams?.length === 0 &&
-                    <div className={styles.noTeam}>
-                        <NotFoundSvg />
+	return (
+		<>
+			<Navbar />
+			<div className={styles.container}>
+				{user?.teams?.length === 0 && (
+					<div className={styles.noTeam}>
+						<NotFoundSvg />
 
-                        <div className={styles.text}>
-                            <span>Nie jesteś członkiem żadnego Zespołu!</span>
-                            <span style={{fontSize: 18}}>Stwórz nowy zespół lub poczekaj na zaproszenie od innego członka retromachiny!</span>
-                            <Button
-                                onClick={() => navigate("/team/create")}
-                                size="small"
-                            >
-                                <CreateTeamSvg />
-                                Stwórz Zespół
-                            </Button>
-                        </div>
-                    </div>
-                }
+						<div className={styles.text}>
+							<span>Nie jesteś członkiem żadnego Zespołu!</span>
+							<span style={{ fontSize: 18 }}>
+								Stwórz nowy zespół lub poczekaj na zaproszenie od innego członka
+								retromachiny!
+							</span>
+							<Button onClick={() => navigate("/team/create")} size="small">
+								<CreateTeamSvg />
+								Stwórz Zespół
+							</Button>
+						</div>
+					</div>
+				)}
 
-                {user?.teams?.map((team) =>
-                    <TeamRetroList
-                        key={team.id}
-                        teamId={team.id}
-                        teamName={team.name} />
-                )}
-            </div>
-        </>
-    )
+				<div className={styles.teams}>
+					{user?.teams?.map((team) => (
+						<TeamRetroList
+							key={team.id}
+							teamId={team.id}
+							teamName={team.name}
+						/>
+					))}
+					<Card
+						teamUsers={[]}
+						author={{
+							id: "aaa",
+							avatar: "assets/sample.png",
+							name: "John Doe",
+						}}
+						text={"Test"}
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default HomeView;

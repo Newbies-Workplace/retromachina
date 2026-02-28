c6f739c5676928cdd13ca87255c04b0a5668c316
JUtupe <jutuper@gmail.com>
2026-02-28 01:24:34 +0100

    fix: google select account prompt

--- apps/api/src/auth/google/google.guard.ts
+++ apps/api/src/auth/google/google.guard.ts
@@ -1,3 +1,9 @@
 import { AuthGuard } from "@nestjs/passport";
 
-export class GoogleGuard extends AuthGuard("google") {}
+export class GoogleGuard extends AuthGuard("google") {
+  constructor() {
+    super({
+      prompt: "select_account",
+    });
+  }
+}

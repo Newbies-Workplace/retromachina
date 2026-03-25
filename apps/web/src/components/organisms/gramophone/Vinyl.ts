7f87bcf3bb15e109916959c3c89c9fd76a4c574f
JUtupe <jutuper@gmail.com>
2026-03-25 01:45:07 +0100

    fix: vinyl sound imports

--- apps/web/src/components/organisms/gramophone/Vinyl.ts
+++ apps/web/src/components/organisms/gramophone/Vinyl.ts
@@ -1,3 +1,7 @@
+import elevatorSound from "@/assets/vinyl/elevator.mp3";
+import focusSound from "@/assets/vinyl/focus.mp3";
+import lofiSound from "@/assets/vinyl/lofi.mp3";
+
 export type Vinyl = {
   id: string;
   name: string;
@@ -12,20 +16,20 @@ export const VINYLS: Vinyl[] = [
     name: "Lofi",
     author: "BFCMUSIC",
     color: "#143985",
-    path: "/assets/vinyl/lofi.mp3",
+    path: lofiSound,
   },
   {
     id: "2",
     name: "Focus",
     author: "FreeGroove",
     color: "#8B4513",
-    path: "/assets/vinyl/focus.mp3",
+    path: focusSound,
   },
   {
     id: "3",
     name: "Elevator",
     author: "Ievgen Poltavskyi",
     color: "#FF6347",
-    path: "/assets/vinyl/elevator.mp3",
+    path: elevatorSound,
   },
 ];

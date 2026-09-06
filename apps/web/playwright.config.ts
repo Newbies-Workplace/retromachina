2da89028f546e6188c303a57f65b99bd470ff92b
JUtupe <jutuper@gmail.com>
2026-09-06 18:56:35 +0200

    chore: config restored

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -5,14 +5,14 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  // retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? "list" : "html",
   use: {
     baseURL: "http://localhost:8080",
-    trace: "retain-on-failure",
+    trace: "on-first-retry",
   },
-  timeout: 5_000,
+  timeout: 15_000,
   projects: [
     // Setup project
     { name: "setup", testMatch: /.*\.setup\.ts/ },
